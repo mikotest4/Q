5fb69122e18f529ecc260c3e323ca18df339b811
mikotest4 <fakealok756@gmail.com>
2025-07-29 11:58:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 
 class Config:
 
-    BOT_TOKEN = "7484678112:AAGz0SfUfOZgKF10v_syfwvO35d2765XBhU"
+    BOT_TOKEN = "7819249411:AAHBwacCoXZl71xq8yF1bkqgxqzTkoFPCYA"
     APP_ID = 27999679
     API_HASH = "f553398ca957b9c92bcb672b05557038"
 
     #comma seperated user id of users who are allowed to use
-    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1423807625,1048110820,6520490787,7100701721,7297547385').split(',')]
+    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','7970350353').split(',')]
  # Absolute path to the folder where you keep your .ttf/.otf files
     FONTS_DIR = os.path.join(os.getcwd(), "fonts")
     
