c87a0a714f84646880294ed871e349a17dd66dc9
mikotest4 <fakealok756@gmail.com>
2025-07-29 19:17:26 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,4 +1,3 @@
-
 # (c) mohdsabahat
 
 #Logging
@@ -10,10 +9,45 @@ logger = logging.getLogger(__name__)
 
 import os
 import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from chat import Chat
 from config import Config
 logging.getLogger('pyrogram').setLevel(logging.WARNING)
 
+async def insert(user_id):
+    """Insert or update user in database - placeholder function"""
+    logger.info(f"User {user_id} accessed the bot")
+    pass
+
+@Client.on_message(filters.command("start") & filters.private)
+async def strtCap(bot, message):
+    user_id = int(message.from_user.id)
+    await insert(user_id)
+
+    # Get bot info to access username
+    bot_info = await bot.get_me()
+    bot_username = bot_info.username
+
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("ʏ", url="https://t.me/Yae_X_Miko"),
+                InlineKeyboardButton("ᴀ", url="https://t.me/Yae_X_Miko"),
+                InlineKeyboardButton("ᴇ", url="https://t.me/Yae_X_Miko"),
+                InlineKeyboardButton("ᴍ", url="https://t.me/Yae_X_Miko"),
+                InlineKeyboardButton("ɪ", url="https://t.me/Yae_X_Miko"),
+                InlineKeyboardButton("ᴋ", url="https://t.me/Yae_X_Miko"),
+                InlineKeyboardButton("ᴏ", url="https://t.me/Yae_X_Miko")
+            ]
+        ]
+    )
+
+    await message.reply_photo(
+        photo=Config.YAE_MIKO_PIC,
+        caption=f"<b>Hᴇʟʟᴏ {message.from_user.mention}\n\nɪ ᴀᴍ sᴜʙᴛɪᴛʟᴇ ᴍɪxᴇʀ ʙᴏᴛ ᴡɪᴛʜ ᴄᴜsᴛᴏᴍ sᴜʙᴛɪᴛʟᴇ.</b>",
+        reply_markup=keyboard
+    )
 
 @pyrogram.Client.on_message(pyrogram.filters.command(['help']))
 async def help_user(bot, update):
@@ -32,19 +66,3 @@ async def help_user(bot, update):
             Chat.NO_AUTH_USER,
             reply_to_message_id = update.id
         )
-
-@pyrogram.Client.on_message(pyrogram.filters.command(['start']))
-async def start(bot, update):
-
-    if str(update.from_user.id) not in Config.ALLOWED_USERS:
-        return await bot.send_message(
-            update.chat.id,
-            Chat.NO_AUTH_USER,
-            reply_to_message_id = update.id
-        )
-
-    await bot.send_message(
-        chat_id = update.chat.id,
-        text = Chat.START_TEXT,
-        reply_to_message_id = update.id
-    )
